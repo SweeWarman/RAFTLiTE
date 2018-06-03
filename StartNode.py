ed32452371e5fbc07cdd1576f5d03c13e9827aa4
swee <swee.balachandran@nianet.org>
2018-06-03 17:02:28 -0400

    Refactored changes for Comm module

--- StartNode.py
+++ StartNode.py
@@ -1,39 +1,19 @@
 from states.neutral import Neutral
 from servers.server import ServerDeamon
-import sys
-import lcm
+from Communication.Comm import *
+from Communication.LcmServer import *
 
 name = sys.argv[1]
 
-_lcm = lcm.LCM()
-
+board = MsgBoard()
 state = Neutral()
-node = ServerDeamon(name,state,[],_lcm)
+
+node = ServerDeamon(name,state,[],board)
 node.daemon = True
 
-_lcm_heartbeat_handler   = lambda channel,data:node.HandleHeartBeat(channel,data)
-_lcm_appendentry_handler = lambda channel,data:node.HandleAppendEntries(channel,data)
-_lcm_requestvote_handler = lambda channel,data:node.HandleRequestVote(channel,data)
-_lcm_response_handler    = lambda channel,data:node.HandleResponse(channel,data)
-_lcm_voteresponse_handler= lambda channel,data:node.HandleVoteResponse(channel,data)
-_lcm_membership_handler  = lambda channel,data:node.HandleMemberShip(channel,data)
-_lcm_clientstatus_handler= lambda channel,data:node.HandleClientStatus(channel,data)
+lcm = LcmServer(node._server,board)
 
-
-_lcm.subscribe("HEARTBEAT",_lcm_heartbeat_handler)
-_lcm.subscribe(name + "_APPEND_ENTRIES",_lcm_appendentry_handler)
-_lcm.subscribe(name + "_REQUEST_VOTE",_lcm_requestvote_handler)
-_lcm.subscribe(name + "_VOTE_RESPONSE",_lcm_voteresponse_handler)
-_lcm.subscribe(name + "_RESPONSE",_lcm_response_handler)
-_lcm.subscribe(name + "_MEMBERSHIP",_lcm_membership_handler)
-_lcm.subscribe(name + "_CLIENT_STATUS",_lcm_clientstatus_handler)
-
-
+lcm.start()
 node.start()
 
-try:
-    while True:
-        _lcm.handle()
-except KeyboardInterrupt:
-    print "Exiting lcm thread"
 
