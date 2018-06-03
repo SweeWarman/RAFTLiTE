7453503c133a95076acf1801cfa2e00fd347cadd
swee <swee.balachandran@nianet.org>
2018-06-03 17:01:51 -0400

    Added messages package

---
+++ Messages/messages.py
@@ -0,0 +1,48 @@
+
+class base(object):
+    def __init__(self):
+        self.timestamp = 0
+        self.sender = ""
+        self.receiver = ""
+
+class append_entries(base):
+    def __init__(self):
+        self.term = 0
+        self.nodes = 0
+        self.nodeID = [ "" for dim0 in range(10) ]
+        self.entryType = 0
+        self.leaderCommit = 0
+        self.logIndex = 0
+        self.prevLogIndex = 0
+        self.prevLogTerm = 0
+        self.data = [ 0.0 for dim0 in range(10) ]
+
+class client_status(base):
+    def __init__(self):
+        self.data = [ 0.0 for dim0 in range(10) ]
+
+class heartbeat(base):
+    def __init__(self):
+        self.timeStamp = 0
+        self.sender = ""
+        self.receiver = ""
+
+class request_membership(base):
+    def __init__(self):
+        self.request = False
+
+class request_vote_t(base):
+
+    def __init__(self):
+        self.term = 0
+
+class response_t(base):
+    def __init__(self):
+        self.data = False
+
+class vote_response_t(base):
+    def __init__(self):
+        self.term = 0
+        self.data = False
+
+
